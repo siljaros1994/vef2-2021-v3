cc90629fb6a59308f6b4b2e05bfdb50631446019
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-25 17:37:18 +0000

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,2 +1,41 @@
 import { promises } from 'fs';
 import faker from 'faker';
+import util from 'util';
+import { query } from './db.js';
+
+require('dotenv').config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const readFileAsync = util.promisify(fs.readFile);
+
+async function main() {
+  console.info(`Set upp gagnagrunn á ${connectionString}`);
+  // droppa töflum ef til
+  await query('DROP TABLE IF EXISTS applications');
+  await query('DROP TABLE IF EXISTS users');
+  console.info('Töflum eytt');
+
+  // búa til töflur út frá skema
+  try {
+    const createTable = await readFileAsync('./schema.sql');
+    await query(createTable.toString('utf8'));
+    console.info('Töflur búnar til');
+  } catch (e) {
+    console.error('Villa við að búa til töflur:', e.message);
+    return;
+  }
+
+  // bæta færslum við töflur
+  try {
+    const insert = await readFileAsync('./insert.sql');
+    await query(insert.toString('utf8'));
+    console.info('Gögnum bætt við');
+  } catch (e) {
+    console.error('Villa við að bæta gögnum við:', e.message);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+});
