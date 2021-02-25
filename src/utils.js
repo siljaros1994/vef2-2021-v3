c7dc115a525f3a59f3844a346f58491f1a697fc6
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-25 17:37:23 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const xss = require('xss');
+
 /**
  * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
  *
@@ -8,4 +10,60 @@ export function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+/**
+ * Sér til þess að það sé notandi innskráður í request.
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {funcion} next Næsta middleware
+ */
+function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
+  return res.redirect('/login');
+}
+
+/**
+ * Sér til þess að innskráður notandi sé admin.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {funcion} next Næsta middleware
+ */
+function ensureAdmin(req, res, next) {
+  if (req.isAuthenticated() && req.user && req.user.admin) {
+    return next();
+  }
+
+  return res.redirect('/login');
+}
+
+/**
+ * Hjálparfall sem XSS hreinsar reit í formi eftir heiti.
+ *
+ * @param {string} fieldName Heiti á reit
+ * @returns {function} Middleware sem hreinsar reit ef hann finnst
+ */
+function sanitizeXss(fieldName) {
+  return (req, res, next) => {
+    if (!req.body) {
+      next();
+    }
+
+    const field = req.body[fieldName];
+
+    if (field) {
+      req.body[fieldName] = xss(field);
+    }
+
+    next();
+  };
+}
+
+module.exports = {
+  catchErrors,
+  ensureLoggedIn,
+  ensureAdmin,
+  sanitizeXss,
+};
