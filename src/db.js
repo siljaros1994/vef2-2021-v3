390dece96efc3700046bdfcbc2e62798bde492f2
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-25 17:37:16 +0000

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,6 +8,11 @@ const {
   NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
+if (!connectionString) {
+  console.error('Vantar DATABASE_URL');
+  process.exit(1);
+}
+
 // Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
 const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
 
@@ -29,4 +34,61 @@ export async function query(_query, values = []) {
   }
 }
 
-// TODO rest af föllum
+/**
+ * Insert a single registration into the registration table.
+ *
+ * @param {string} entry.name – Name of registrant
+ * @param {string} entry.nationalId – National ID of registrant
+ * @param {string} entry.comment – Comment, if any from registrant
+ * @param {boolean} entry.anonymous – If the registrants name should be displayed or not
+ * @returns {Promise<boolean>} Promise, resolved as true if inserted, otherwise false
+ */
+export async function insert({
+  name, nationalId, comment, anonymous,
+} = {}) {
+  let success = true;
+
+  const q = `
+    INSERT INTO signatures
+      (name, nationalId, comment, anonymous)
+    VALUES
+      ($1, $2, $3, $4);
+  `;
+  const values = [name, nationalId, comment, anonymous === 'on'];
+
+  try {
+    await query(q, values);
+  } catch (e) {
+    console.error('Error inserting signature', e);
+    success = false;
+  }
+
+  return success;
+}
+
+/**
+ * List all registrations from the registration table.
+ *
+ * @returns {Promise<Array<list>>} Promise, resolved to array of all registrations.
+ */
+export async function list() {
+  let result = [];
+  try {
+    const queryResult = await query(
+      'SELECT name, nationalId, comment, anonymous, signed FROM signatures ORDER BY signed DESC',
+    );
+
+    if (queryResult && queryResult.rows) {
+      result = queryResult.rows;
+    }
+  } catch (e) {
+    console.error('Error selecting signatures', e);
+  }
+
+  return result;
+}
+
+// Helper to remove pg from the event loop
+export async function end() {
+  await pool.end();
+}
