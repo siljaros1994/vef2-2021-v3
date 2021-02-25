bbc0df5dd71d12d0b5e2e46f38f288bd6235dc99
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-25 17:37:08 +0000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,4 +11,13 @@ CREATE TABLE IF NOT EXISTS signatures(
   signed timestamp with time zone not null default current_timestamp
 );
 
--- TODO setja inn töflu fyrir notendur
+CREATE TABLE users (
+  id serial primary key,
+  username character varying(255) UNIQUE NOT NULL,
+  password character varying(255) NOT NULL,
+  name varchar(128) not null,
+  email varchar(256) not null,
+  admin boolean default false,
+  created timestamp with time zone not null default current_timestamp,
+  updated timestamp with time zone not null default current_timestamp
+);
