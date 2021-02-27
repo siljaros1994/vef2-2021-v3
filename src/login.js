6f44d4be4fc200b15c68926bae3738ffe30d478a
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-02-27 21:46:12 +0000

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,6 +1,54 @@
 import passport from 'passport';
 import { Strategy } from 'passport-local';
+import { comparePasswords, findByUsername } from './users.js';
 
 // Hægt að útfæra passport virkni hér til að létta á app.js
+/**
+ * Athugar hvort username og password sé til í notandakerfi.
+ * Callback tekur við villu sem fyrsta argument, annað argument er
+ * - `false` ef notandi ekki til eða lykilorð vitlaust
+ * - Notandahlutur ef rétt
+ *
+ * @param {string} username Notandanafn til að athuga
+ * @param {string} password Lykilorð til að athuga
+ * @param {function} done Fall sem kallað er í með niðurstöðu
+ */
+async function strat(username, password, done) {
+    try {
+      const user = await findByUsername(username);
+  
+      if (!user) {
+        return done(null, false);
+      }
+  
+      // Verður annað hvort notanda hlutur ef lykilorð rétt, eða false
+      const result = await comparePasswords(password, user);
+      return done(null, result);
+    } catch (err) {
+      console.error(err);
+      return done(err);
+    }
+  }
+  
+  // Notum local strategy með „strattinu“ okkar til að leita að notanda
+  passport.use(new Strategy(strat));
+  
+  // getum stillt með því að senda options hlut með
+  // passport.use(new Strategy({ usernameField: 'email' }, strat));
+  
+  // Geymum id á notanda í session, það er nóg til að vita hvaða notandi þetta er
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  // Sækir notanda út frá id
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
 
 export default passport;
